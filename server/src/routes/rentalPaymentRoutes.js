e75060b5cdc571598fe45bb346fb3ae896511082
Clever Engineers <thecleverengineers@gmail.com>
2026-09-26 17:46:02 +0530

    Wire landlord transaction and proof routes

--- server/src/routes/rentalPaymentRoutes.js
+++ server/src/routes/rentalPaymentRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { authenticate } from '../middleware/auth.js';
-import { acceptRentalPayment, rejectRentalPayment, submitRentalInvoicePayment } from '../controllers/rentalPaymentController.js';
+import { acceptRentalPayment, getRentalPaymentProof, listLandlordTransactions, rejectRentalPayment, submitRentalInvoicePayment } from '../controllers/rentalPaymentController.js';
 
 const router = Router();
 router.use(authenticate);
+router.get('/landlord-transactions', listLandlordTransactions);
 router.post('/invoices/:invoiceId/payment', submitRentalInvoicePayment);
+router.get('/payments/:paymentId/proof', getRentalPaymentProof);
 router.post('/payments/:paymentId/accept', acceptRentalPayment);
 router.post('/payments/:paymentId/reject', rejectRentalPayment);
 
